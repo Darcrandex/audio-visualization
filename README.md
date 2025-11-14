928e96722f38bf9358219a29fcc970270cbfc580
darcrand <darcrandex@gmail.com>
2025-11-14 12:38:24 +0800

    ✨ feat(app): header 添加

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# audio-visualization
+# Sound Vizor
 
 > 用于可视化音频文件, 展示或者录屏用途
--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import MusicVisualizer from './components/MusicVisualizer'
 export default function App() {
   return (
     <ConfigProvider theme={{ token: { colorPrimary: '#34d399' }, algorithm: theme.darkAlgorithm }}>
+      <header className='fixed top-2 left-2 flex items-center'>
+        <img src='/logo-512.png' className='w-10 h-10 mr-4 object-contain' />
+        <h1 className='font-silent-forest text-4xl text-white'>Sound Vizor</h1>
+      </header>
+
       <MusicVisualizer />
     </ConfigProvider>
   )
